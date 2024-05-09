4b6341a286e17efb726a6c0bf7a22e7a81e9e194
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2024-05-09 18:51:28 -0500

    Create providers

---
+++ Pages/Providers/Create.cshtml.cs
@@ -0,0 +1,40 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+{
+	public class CreateModel : PageModel
+	{
+		private readonly SupermarketContext _context;
+
+		public CreateModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		[BindProperty]
+
+		public Provider Provider { get; set; } = default!;
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid || _context.Provider == null || Provider == null)
+			{
+				return Page();
+			}
+
+			_context.Provider.Add(Provider);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
