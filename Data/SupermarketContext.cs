b4edb75c3d68dc9a57bf8a426f3384b044fdb1e6
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2024-05-09 18:48:23 -0500

    index Providers

--- Data/SupermarketContext.cs
+++ Data/SupermarketContext.cs
@@ -12,5 +12,14 @@ namespace SupermarketWEB.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-    }
+		public DbSet<Provider> Provider { get; set; }
+
+        public DbSet<payMode> payMode { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(
+				"Server=(localdb)\\mssqllocaldb;Database=SupermarketEF;Trusted_Connection=True;");
+		}
+
+	}
 }
---
+++ Models/payMode.cs
@@ -0,0 +1,9 @@
+﻿namespace SupermarketWEB.Models
+{
+	public class payMode
+	{
+		public int id {  get; set; }
+		public string name { get; set; }
+		public string Observation { get; set; }	
+	}
+}
