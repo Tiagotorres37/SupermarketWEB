880965377d1a4b7e2e483d0894e4cbfff201e2b8
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2024-05-09 19:32:56 -0500

    Create PayMode

---
+++ Pages/PayModes/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.PayModes
+{
+	public class CreateModel : PageModel
+	{
+		private readonly SupermarketContext _context;
+
+		public CreateModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+		[BindProperty]
+
+		public payMode PayMode { get; set; } = default!;
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid || _context.payMode == null || PayMode == null)
+			{
+				return Page();
+			}
+
+			_context.payMode.Add(PayMode);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
